281def4844e2a314c27d0f694932b8debd32d14f
Ashrateppey Q <nurshidinnarimanov@gmail.com>
2026-01-29 14:40:48 +0500

    Add files via upload

---
+++ lab1/src/StudentInfo.java
@@ -0,0 +1,20 @@
+public git commiyclass StudentInfo {
+
+    public static void main(String[] args) {
+
+        String fullName = "Нуршидин Нариманов";
+
+        String group = "ИС-24-21";
+
+        String program = "Информационные системы";
+
+        System.out.println("=== Информация о студенте ===");
+
+        System.out.println("ФИО: " + fullName);
+
+        System.out.println("Группа: " + group);
+
+        System.out.println("ОП/Специальность: " + program);
+    }
+}
+
